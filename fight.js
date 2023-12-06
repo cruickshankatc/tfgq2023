5b25f9cd7fac11c030409e4c870e196011a43ca5
Andrew Cruickshank <cruickshankatc@gmail.com>
2023-12-06 00:17:04 -0500

    Added second card and most of card styling.

--- fight.js
+++ fight.js
@@ -1,7 +1,8 @@
 //-------------------SETUP-------------------//
 //Merely gets the id for the empty <section> with the id "battleground"
 let battleground = document.getElementById("battleground");
-
+let cardCharacter;
+let cardCharacter2;
 //Creates an array
 let squareSpaces = []; 
 
@@ -247,7 +248,8 @@ let displayChanges = function() {
 	}
 }
 displayChanges();
-cardUpdate();
+cardUpdate1();
+cardUpdate2();
 
 
 
@@ -751,7 +753,8 @@ let moveAction = function(x) {
 
 	playerCounterUpdate();
 	displayChanges();
-	cardUpdate();
+	cardUpdate1();
+	cardUpdate2();
 }
 
 
@@ -804,7 +807,8 @@ function firepowerAction(xz) {
 	}
 
 	displayChanges();
-	cardUpdate();
+	cardUpdate1();
+	cardUpdate2();
 
 	enemyTarget = [];
 	enemyCounter = 0;
@@ -848,16 +852,26 @@ function playerCounterUpdate() {
 
 
 
-function cardUpdate () {
+
+function cardUpdate1 () {
+	let card1 = document.getElementById("stats1");
+	if (playerCounter === 0 || playerCounter % 2 === 0) {
+		cardCharacter = playerCounter;
+		card1.classList.remove("waiting");
+	} else {
+		card1.classList.add("waiting");
+	}
+	
 	let health1 = document.getElementById("health1");
 	let ultimate1 = document.getElementById("ultimate1");
 	let firepower1 = document.getElementById("firepower1");
 	let speed1 = document.getElementById("speed1");
 	let range1 = document.getElementById("range1");
-
-	document.getElementById("name1").innerHTML = theCharacters[playerCounter].name;
+	
+	console.log(cardCharacter);
+	document.getElementById("name1").innerHTML = theCharacters[cardCharacter].name;
 	let cardImage = document.getElementById("card1").querySelector("img");
-	cardImage.src = "images/cards/" + theCharacters[playerCounter].name +".png";
+	cardImage.src = "images/cards/" + theCharacters[cardCharacter].name +".png";
 
 
 	while (health1.firstChild) {
@@ -881,9 +895,9 @@ function cardUpdate () {
 	}
 
 
-	for (x = 0; x < theCharacters[playerCounter].health; x++) {
+	for (x = 0; x < theCharacters[cardCharacter].health; x++) {
 		let square = document.createElement("div");
-		if (x < theCharacters[playerCounter].currentHealth) {
+		if (x < theCharacters[cardCharacter].currentHealth) {
 			square.classList.add("fullHealth");
 		} else {
 			square.classList.add("emptyHealth");
@@ -891,9 +905,9 @@ function cardUpdate () {
 		health1.appendChild(square);
 	}
 
-	for (x = 0; x < theCharacters[playerCounter].ultimate; x++) {
+	for (x = 0; x < theCharacters[cardCharacter].ultimate; x++) {
 		let square = document.createElement("div");
-		if (x < theCharacters[playerCounter].currentUltimate) {
+		if (x < theCharacters[cardCharacter].currentUltimate) {
 			square.classList.add("fullHealth");
 		} else {
 			square.classList.add("emptyHealth");
@@ -903,7 +917,7 @@ function cardUpdate () {
 
 	for (x = 0; x < 10; x++) {
 		let square = document.createElement("div");
-		if (x < theCharacters[playerCounter].firepower) {
+		if (x < theCharacters[cardCharacter].firepower) {
 			square.classList.add("fullHealth");
 		} else {
 			square.classList.add("emptyHealth");
@@ -914,7 +928,7 @@ function cardUpdate () {
 	
 	for (x = 0; x < 10; x++) {
 		let square = document.createElement("div");
-		if (x < theCharacters[playerCounter].speed) {
+		if (x < theCharacters[cardCharacter].speed) {
 			square.classList.add("fullHealth");
 		} else {
 			square.classList.add("emptyHealth");
@@ -924,7 +938,7 @@ function cardUpdate () {
 
 	for (x = 0; x < 10; x++) {
 		let square = document.createElement("div");
-		if (x < theCharacters[playerCounter].range) {
+		if (x < theCharacters[cardCharacter].range) {
 			square.classList.add("fullHealth");
 		} else {
 			square.classList.add("emptyHealth");
@@ -935,3 +949,102 @@ function cardUpdate () {
 
 
 
+
+
+
+
+function cardUpdate2 () {
+	let card2 = document.getElementById("stats2");
+	if (playerCounter === 0) {
+		cardCharacter2 = 1;
+		card2.classList.add("waiting");
+	} else if (playerCounter % 2 !== 0) {
+		cardCharacter2 = playerCounter;
+		card2.classList.remove("waiting");
+	} else {
+		card2.classList.add("waiting");
+	}
+
+	let health2 = document.getElementById("health2");
+	let ultimate2 = document.getElementById("ultimate2");
+	let firepower2 = document.getElementById("firepower2");
+	let speed2 = document.getElementById("speed2");
+	let range2 = document.getElementById("range2");
+
+	document.getElementById("name2").innerHTML = theCharacters[cardCharacter2].name;
+	let cardImage = document.getElementById("card2").querySelector("img");
+	cardImage.src = "images/cards/" + theCharacters[cardCharacter2].name +".png";
+
+
+	while (health2.firstChild) {
+    health2.removeChild(health2.firstChild);
+	}
+
+	while (ultimate2.firstChild) {
+    ultimate2.removeChild(ultimate2.firstChild);
+	}
+	
+	while (firepower2.firstChild) {
+    firepower2.removeChild(firepower2.firstChild);
+	}
+
+	while (speed2.firstChild) {
+    speed2.removeChild(speed2.firstChild);
+	}
+
+	while (range2.firstChild) {
+    range2.removeChild(range2.firstChild);
+	}
+
+
+	for (x = 0; x < theCharacters[cardCharacter2].health; x++) {
+		let square = document.createElement("div");
+		if (x < theCharacters[cardCharacter2].currentHealth) {
+			square.classList.add("fullHealth");
+		} else {
+			square.classList.add("emptyHealth");
+		}
+		health2.appendChild(square);
+	}
+
+	for (x = 0; x < theCharacters[cardCharacter2].ultimate; x++) {
+		let square = document.createElement("div");
+		if (x < theCharacters[cardCharacter2].currentUltimate) {
+			square.classList.add("fullHealth");
+		} else {
+			square.classList.add("emptyHealth");
+		}
+		ultimate2.appendChild(square);
+	}
+
+	for (x = 0; x < 10; x++) {
+		let square = document.createElement("div");
+		if (x < theCharacters[cardCharacter2].firepower) {
+			square.classList.add("fullHealth");
+		} else {
+			square.classList.add("emptyHealth");
+		}
+		firepower2.appendChild(square);
+	}
+
+	
+	for (x = 0; x < 10; x++) {
+		let square = document.createElement("div");
+		if (x < theCharacters[cardCharacter2].speed) {
+			square.classList.add("fullHealth");
+		} else {
+			square.classList.add("emptyHealth");
+		}
+		speed2.appendChild(square);
+	}
+
+	for (x = 0; x < 10; x++) {
+		let square = document.createElement("div");
+		if (x < theCharacters[cardCharacter2].range) {
+			square.classList.add("fullHealth");
+		} else {
+			square.classList.add("emptyHealth");
+		}
+		range2.appendChild(square);
+	}
+}
