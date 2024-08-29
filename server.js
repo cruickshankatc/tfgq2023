6ea0fb639309d23d91bedcad8342a30a8fc46846
Andrew Cruickshank <cruickshankatc@gmail.com>
2024-08-29 14:05:51 -0400

    Created a 'public' folder, and placed static files within.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join()))
